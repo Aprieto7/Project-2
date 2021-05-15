9beeae2a6475df01facdbdd551bb2ac0dfbda545
tnguyen0306 <tnguyen0306@gmail.com>
2021-05-15 14:20:33 -0500

    5-15 2:20pm use d3 to call json data

---
+++ static/js/logic.js
@@ -0,0 +1,9 @@
+var queryUrlStates = `https://api.covidactnow.org/v2/states.json?apiKey=${API_KEY}`
+var queryUrlCounties = `https://api.covidactnow.org/v2/counties.json?apiKey=${API_KEY}`
+var queryUrlCbsas = `https://api.covidactnow.org/v2/cbsas.json?apiKey=${API_KEY}`
+
+// Create functions to make d3.json works
+
+d3.json("link").then(function(response) {
+  createMarkers(response);
+});
