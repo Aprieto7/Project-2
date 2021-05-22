fa741598595c11851c18ee7de70e2a51ac9322a5
Adolfo <adolfoprieto37@yahoo.com>
2021-05-22 13:54:55 -0500

    cleaned syntax

--- static/js/app.js
+++ static/js/app.js
@@ -18,69 +18,71 @@ var queryUrlCbsas = `https://api.covidactnow.org/v2/cbsas.json?apiKey=${API_KEY}
 
 function buildCharts(vaccination) {
     d3.json("data/covid.db").then((Data) => {
-  //Point to the vaccination portion of the data file
-      var vaccinations = Data.vaccinations;
+    //Point to the vaccination portion of the data file
+        var vaccinations = Data.vaccinations;
 
-      var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
-      var result = resultArray[0];
+        var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
+        var result = resultArray[0];
 
-  //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
-      var states = result.state;
-      var vaccinationsCompleted = result.VaccinationsCompleted;
-      var vaccinationsDistributed = result.vaccinationsDistributed.reverse();
-      var vaccinationsAdministered = result.vaccinationsAdministered;
+    //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
+        var state = result.state;   
+        var vaccinationsCompleted = result.VaccinationsCompleted;
+        var vaccinesDistributed = result.vaccinesDistributed.reverse();
+        var vaccinesAdministered = result.vaccinessAdministered;
+    
 
-//Create Gauge Chart 
-var data = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value: vaccinationsCompleted,
-      title: { text: 'Fully Vaccinated Percentage'},
-      type: 'indicator',
-      mode: 'gauge+number',
-      gauge: {
-        axis: { range: [null, 100] },
-        steps: [
-          { range: [0, 25], color: 'red',
-             range: [25, 50], color: 'orange',
-            range: [50, 75], color: 'yellow',
-            range: [75, 100], color: 'green'}
-        ],
-        threshold: {
-          line: { color: 'gold', width: 4 },
-          thickness: 0.75,
-          value: 99
+    //Create Gauge Chart 
+    var data = [
+        {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: vaccinationsCompleted,
+        title: { text: 'Fully Vaccinated Percentage'},
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+            axis: { range: [null, 100] },
+            steps: [
+            { range: [0, 25], color: 'red',
+                range: [25, 50], color: 'orange',
+                range: [50, 75], color: 'yellow',
+                range: [75, 100], color: 'green'}
+            ],
+            threshold: {
+            line: { color: 'gold', width: 4 },
+            thickness: 0.75,
+            value: 99
+                }
             }
         }
-    }
-];
-  
-  var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
-  
-  Plotly.newPlot('myGuage', data, layout);
-  
-  //Stacked Bar Chart
+    ];
+    
+    var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    
+    Plotly.newPlot('myGuage', data, layout);
+    
+    //Stacked Bar Chart
 
-  var trace1 = {
-    x: state,
-    y: vaccinationsAdministered,
-    name: 'Vaccinations Administered',
-    type: 'bar'
-  };
-  
-  var trace2 = {
-    x: state,
-    y: vaccinationsDistributed,
-    name: 'Vaccinations Distributed',
-    type: 'bar'
-  };
-  
-  var data = [trace1, trace2];
-  
-  var layout = {barmode: 'stack'};
-  
-  Plotly.newPlot('myAreaChart', data, layout);
-
+    var trace1 = {
+        x: state,
+        y: vaccinesAdministered,
+        name: 'Vaccinations Administered',
+        type: 'bar'
+    };
+    
+    var trace2 = {
+        x: state,
+        y: vaccinesDistributed,
+        name: 'Vaccinations Distributed',
+        type: 'bar'
+    };
+    
+    var data = [trace1, trace2];
+    
+    var layout = {barmode: 'stack'};
+    
+    Plotly.newPlot('myAreaChart', data, layout);
+    })
+};
 
 function init() {  
         // call the functions to display the data and the plots to the page
