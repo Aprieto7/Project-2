95f61176eb29e9ad1b8562f0a0c055db87c5c0fd
Brandy Knust <brandy_1175@hotmail.com>
2021-05-23 11:36:35 -0500

    brandy 5-23 am

--- Flask_Setup/app.py
+++ Flask_Setup/app.py
@@ -5,7 +5,11 @@ import requests as r
 from flask_pymongo import PyMongo
 import sqlalchemy
 
-engine=sqlalchemy.create_engine('sqlite:///data.db')
+engine = sqlalchemy.create_engine('sqlite:///covid.db')
+df1 = pd.read_sql_table(sqlite_table1)
+df2 = pd.read_sql_table(sqlite_table2)
+df1.to_sql("historical", index=False, con=engine)
+df2.to_sql("states", index=False, con=engine)
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -16,6 +20,8 @@ app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/app_name")
 
 # Route to render index.html template using data from Mongo
+
+
 @app.route("/")
 def home():
 
@@ -24,12 +30,38 @@ def home():
     # # Return template and data
     return render_template("index.html")
 
+
 @app.route("/data")
 def jsonified():
-    
-    
 
+    rows = engine.execute(
+        "SELECT date, cases, deaths, newCases, vaccinesAdministered FROM historical")
+    res1 = []
+    for row in rows:
+        x = {"date": row[1],
+             "cases": row[2],
+             "deaths": row[3],
+             "newCases": row[4],
+             "vaccinesAdministered": [5]
+             }
+        res1.append(x)
+
+    rows = engine.execute("SELECT state, cases, deaths, positiveTests, newCases, vaccinesDistributed, vaccinationsInitiated, vaccinationsCompleted, vaccinesAdministered, lat, long")
+    res2 = []
+    for row in rows:
+        x = {"state": row[1],
+            "cases": row[2],
+            "deaths": row[3],
+            "positiveTests": row[4],
+            "newCases": row[5],
+            "vaccinesDistributed": row[6],
+            "vaccinationsInitiated": row[7],
+            "vaccinationsCompleted": row[8],
+            "vaccinesAdministered": row[9],
+            "lat": row[10],
+            "long": row[11]
+            }
+        res2.append(x)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
