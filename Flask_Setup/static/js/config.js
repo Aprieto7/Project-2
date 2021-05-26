5c9f419a1e86ec01eea81ea7f32ab50c46036cf3
tnguyen0306 <tnguyen0306@gmail.com>
2021-05-25 19:59:33 -0500

    52521 7:58pm - add color scale and change source

--- Flask_Setup/static/js/config.js
+++ Flask_Setup/static/js/config.js
@@ -1,1 +1,2 @@
 var API_KEY = "4090eb7d45354f25978fc1201f714917";
+const API_KEY_map = "pk.eyJ1IjoiaWFucm95ZGV2IiwiYSI6ImNrb2tocHIxYzBjcTIybm83dHpicnFydmgifQ.H_0tRMQQEtd8nIXZhaK_7g";
--- Flask_Setup/static/js/logic.js
+++ Flask_Setup/static/js/logic.js
@@ -4,7 +4,7 @@ var queryUrlCbsas = `https://api.covidactnow.org/v2/cbsas.json?apiKey=${API_KEY}
 
 // Create functions to make d3.json works
 // Create a map object
-var myMap = L.map("myMap", {
+var myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 4
 });
@@ -20,35 +20,28 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // Define a markerSize function that will give each city a different radius based on its population
 function markerSize(cases) {
-  return cases / 50000;
+  return cases / 10000;
 }
-
-// function createMarkers(response) {
-//   var covid = [];
-
-//   // Loop through the cities array and create one marker for each city object
-//   for (var i = 0; i < response.length; i++) {
-//     var latlong = [response[i].lat,response[i].long];
-//     var markers = L.circleMarker(latlong, {
-//       fillOpacity: 0.2,
-//       color: "gray",
-//       fillColor: "green",
-//       // Adjust radius
-//       radius: 10 //coordinateData[i].cases/50000
-//     }).bindPopup("<h3>" + response[i].state + "</h3> <hr> <h4>Cases: " + response[i].cases + "</h4> <r> <h4>Death: " + response[i].deaths + "</h4>");
-//     covid.push(markers);
-//     L.layerGroup(markers);
-//   }
-// }
 
 function percentage(a,b) {
   var percent = a*100/b;
   return percent
 }
 
+// Create colorScale() function to get the color
+function colorScale(x) {
+  return  x > 0.25 ? "#ff5f65" :
+  x > 0.2 ? "#fca35d" :
+  x > 0.15 ? "#fdb72a" :
+  x > 0.1 ? "#f7db11" :
+  x > 0.05 ? "#dcf400" :
+  x > 0 ? "#a3f600" :
+      "#FFEDA0"
+};
+
 
 // Each city object contains the city's name, location and population
-d3.csv("static/data/df.csv").then(
+d3.csv("/stateData").then(
   function(coordinateData) {
   coordinateData.forEach(function(data) {
       data.lat = +data.lat;
@@ -64,15 +57,34 @@ d3.csv("static/data/df.csv").then(
     var casePer = percentage(coordinateData[i].cases,coordinateData[i].population);
     var deathPer = percentage(coordinateData[i].deaths,coordinateData[i].population)
     L.circleMarker(latlong, {
-      fillOpacity: 0.2,
+      fillOpacity: 0.4,
       color: "gray",
-      fillColor: "green",
+      fillColor: colorScale(deathPer),
       // Adjust radius
       radius: casePer
-    }).bindPopup("<h3>" + coordinateData[i].state + "</h3> <hr> <h4>Cases Percent: " + casePer.toFixed(2) + "%</h4> <r> <h4>Death Percent: " + deathPer.toFixed(2) + "%</h4>").addTo(myMap);
+    }).bindPopup("<h3>" + coordinateData[i].name + "</h3> <hr> <h4>Cases Percent: " + casePer.toFixed(2) + "%</h4> <r> <h4>Death Percent: " + deathPer.toFixed(2) + "%</h4>").addTo(myMap);
   }
-});
 
-// d3.json("link").then(function(response) {
-//   createMarkers(response);
-// });
+  // Create legend 
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function(map) {
+      var div = L.DomUtil.create('div','legend');
+      var depth = [0,0.05,0.1,0.15,0.2,0.25];
+      var labels = [];
+          // Loop through data to push the label of each info
+          for (var i=0; i < depth.length; i++){
+              if (i===5) {
+                  labels.push("<tr><td style = 'text-align: center; padding:3px; margin:3px; background-color:" + colorScale(depth[i]) + "'>" + depth[i] + '%+' + "</td></tr>");
+              } 
+              else {
+                  labels.push("<tr><td style = 'text-align: center; padding:3px; margin:3px; background-color:" + colorScale(depth[i]) + "'>" + depth[i] + '% - ' + depth[i+1] + "%</td></tr>");
+              }
+          }
+      div.innerHTML += "<table style ='background-color: white; border-radius: 4px'><th'></th>" + labels.join("") + "</table>";
+      
+      return div;
+  };
+  // Add the legend to myMap
+  legend.addTo(myMap);
+});
