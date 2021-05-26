bca818045a9928d1b80ca1f0ed848ca99a16db5d
Brandy K <brandy_1175@Hotmail.com>
2021-05-25 21:33:55 -0500

    933pm 5-25

--- Flask_Setup/static/js/app.js
+++ Flask_Setup/static/js/app.js
@@ -7,7 +7,7 @@ var queryUrlCbsas = `https://api.covidactnow.org/v2/cbsas.json?apiKey=${API_KEY}
 
 //     // Import data from the csv file
 //     d3.csv('link').then(function(data) {
-        
+
 // },
 
 // // When the browser loads, makeResponsive() is called.
@@ -16,199 +16,231 @@ var queryUrlCbsas = `https://api.covidactnow.org/v2/cbsas.json?apiKey=${API_KEY}
 // // When the browser window is resized, makeResponsive() is called.
 // d3.select(window).on('resize', makeResponsive);
 
-
+var state = []
 
 function buildCharts(vaccination) {
-    d3.json("/stateData").then((Data) => {
-  //Point to the vaccination portion of the data file
-      var vaccinations = Data.vaccinations;
+  d3.json("/stateData").then((Data) => {
+    //Point to the vaccination portion of the data file
+    var vaccinations = Data.vaccinesAdministered;
+    console.log(vaccinations)
 
-      var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
-      var result = resultArray[0];
+    var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
+    var result = resultArray[0];
 
-      var stateList = Data.state;
-      var dropdownMenu = de.selectAll("#selDataset");
-      stateList.forEach(function (state) {
-        dropdownMenu.append("option").text(state)
+    var stateList = Data.state;
+    var dropdownMenu = d3.selectAll("#selDataset");
+    stateList.forEach(function (state) {
+      dropdownMenu.append("option").text(state)
         .property("value", state)
-      });
+    });
 
-      myBarChart(stateList[0])
-      myAreaChart(stateList[0])
-      myGauge(stateList[0])
-      myMap(stateList[0])
+    myBarChart(stateList[0])
+    myAreaChart(stateList[0])
+    myGauge(stateList[0])
+    myMap(stateList[0])
 
-  //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
-      var states = result.state;
-      var vaccinationsCompleted = result.VaccinationsCompleted;
-      var vaccinationsDistributed = result.vaccinationsDistributed.reverse();
-      var vaccinationsAdministered = result.vaccinationsAdministered;
+    //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
+    var states = result.state;
+    var vaccinationsCompleted = result.VaccinationsCompleted;
+    var vaccinationsDistributed = result.vaccinationsDistributed.reverse();
+    var vaccinationsAdministered = result.vaccinationsAdministered;
 
-//Create Gauge Chart 
-var data = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value: vaccinationsCompleted,
-      title: { text: 'Fully Vaccinated Percentage'},
-      type: 'indicator',
-      mode: 'gauge+number',
-      gauge: {
-        axis: { range: [null, 100] },
-        steps: [
-          { range: [0, 25], color: 'red',
-             range: [25, 50], color: 'orange',
-            range: [50, 75], color: 'yellow',
-            range: [75, 100], color: 'green'}
-        ],
-        threshold: {
-          line: { color: 'gold', width: 4 },
-          thickness: 0.75,
-          value: 99
+    //Create Gauge Chart 
+    var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: vaccinationsCompleted,
+        title: { text: 'Fully Vaccinated Percentage' },
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+          axis: { range: [null, 100] },
+          steps: [
+            {
+              range: [0, 25], color: 'red',
+              range: [25, 50], color: 'orange',
+              range: [50, 75], color: 'yellow',
+              range: [75, 100], color: 'green'
             }
+          ],
+          threshold: {
+            line: { color: 'gold', width: 4 },
+            thickness: 0.75,
+            value: 99
+          }
         }
-    }
-];
-  
-  var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
-  
-  Plotly.newPlot('myGuage', data, layout);
-  
-  //Stacked Bar Chart
+      }
+    ];
 
-  var trace1 = {
-    x: state,
-    y: vaccinationsAdministered,
-    name: 'Vaccinations Administered',
-    type: 'bar'
-  };
-  
-  var trace2 = {
-    x: state,
-    y: vaccinationsDistributed,
-    name: 'Vaccinations Distributed',
-    type: 'bar'
-  };
-  
-  var data = [trace1, trace2];
-  
-  var layout = {barmode: 'stack'};
-  
-  Plotly.newPlot('myAreaChart', data, layout);
+    var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+
+    Plotly.newPlot('myGuage', data, layout);
+
+    //Stacked Bar Chart
+
+    var trace1 = {
+      x: state,
+      y: vaccinationsAdministered,
+      name: 'Vaccinations Administered',
+      type: 'bar'
+    };
+
+    var trace2 = {
+      x: state,
+      y: vaccinationsDistributed,
+      name: 'Vaccinations Distributed',
+      type: 'bar'
+    };
+
+    var data = [trace1, trace2];
+
+    var layout = { barmode: 'stack' };
+
+    Plotly.newPlot('myAreaChart', data, layout);
 
 
-function init() {  
-        // call the functions to display the data and the plots to the page
-        buildCharts(Data.vaccinations[0]);
+
+  })
+}
+
+
+
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
 };
-  
-init();
-    })}
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("#myBarChart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// d3.csv("donuts.csv").then(function(donutData) { 
+
+function buildCharts(filteredState) {
+
+  d3.json("/stateData").then((Data) => {
+    //Point to the vaccination portion of the data file
+    var vaccinations = Data.vaccinesAdministered;
+    console.log(vaccinations)
+
+    Data.forEach(function (d) {
+      var vaccinesAdministered = d.vaccinesAdministered;
+      // var resultArray = vaccinesAdministered.filter(state => state == state);
+      // var result = resultArray[0];
+
+      //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
+      var states = d.state;
+      var cases = d.cases;
+      var date = d.date;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+      .domain(d3.max(Data, d => d.date))
+      .range([0, width]);
+
+    var yLinearScale1 = d3.scaleLinear()
+      .domain([0, d3.max(Data, d => d.vaccinesAdministered)])
+      .range([height, 0]);
+
+    var yLinearScale2 = d3.scaleLinear()
+      .domain([0, d3.max(Data, d => d.cases)])
+      .range([height, 0]);
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale1);
+    var rightAxis = d3.axisRight(yLinearScale2);
+
+    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(bottomAxis);
+
+    // Add leftAxis to the left side of the display
+    chartGroup.append("g").call(leftAxis);
+
+    // Add rightAxis to the right side of the display
+    chartGroup.append("g").attr("transform", `translate(${width}, 0)`).call(rightAxis);
+
+    var line1 = d3
+      .line()
+      .x(d => xLinearScale(d.date))
+      .y(d => yLinearScale1(d.vaccinesAdministered));
+
+    var line2 = d3
+      .line()
+      .x(d => xLinearScale(d.date))
+      .y(d => yLinearScale2(d.cases));
 
 
+    // Append a path for line1
+    chartGroup.append("#myBarChart")
+      .data([Data])
+      .attr("d", line1)
+      .attr("color", "green")
+
+    // Append a path for line2
+    chartGroup.append("myBarChart")
+      .data([Data])
+      .attr("d", line2)
+      .attr("color", "red")
+
+  })
 
 
-  var svgWidth = 960;
-  var svgHeight = 500;
-
-  var margin = {
-      top: 20,
-      right: 40,
-      bottom: 60,
-      left: 50
-  };
-
-  var width = svgWidth - margin.left - margin.right;
-  var height = svgHeight - margin.top - margin.bottom;
-
-  // Step 2: Create an SVG wrapper,
-  // append an SVG group that will hold our chart,
-  // and shift the latter by left and top margins.
-  // =================================
-  var svg = d3
-      .select("#myBarChart")
-      .append("svg")
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
-
-  var chartGroup = svg.append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-  // d3.csv("donuts.csv").then(function(donutData) { 
-
-  function buildCharts(value) {
-      var vaccinations = Data.vaccinesAdministered;
-      var date = Data.date
-      d3.json("/historicalData").then(function (Data) {
-          //Point to the vaccination portion of the data file
-
-
-          Data.forEach(function (Data) {
-              var resultArray = vaccinesAdministered.filter(state => state == state);
-              var result = resultArray[0];
-
-              //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
-              var states = result.state;
-              var vaccinesAdministered = result.vaccinesAdministered;
-              var cases = result.cases;
-              var date = result.date;
-          });
-
-          var xLinearScale = d3.scaleLinear()
-              .domain(d3.max(Data, d => d.date))
-              .range([0, width]);
-
-          var yLinearScale1 = d3.scaleLinear()
-              .domain([0, d3.max(Data, d => d.vaccinesAdministered)])
-              .range([height, 0]);
-
-          var yLinearScale2 = d3.scaleLinear()
-              .domain([0, d3.max(Data, d => d.cases)])
-              .range([height, 0]);
-
-          var bottomAxis = d3.axisBottom(xLinearScale);
-          var leftAxis = d3.axisLeft(yLinearScale1);
-          var rightAxis = d3.axisRight(yLinearScale2);
-
-          chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(bottomAxis);
-
-          // Add leftAxis to the left side of the display
-          chartGroup.append("g").call(leftAxis);
-
-          // Add rightAxis to the right side of the display
-          chartGroup.append("g").attr("transform", `translate(${width}, 0)`).call(rightAxis);
-
-          var line1 = d3
-              .line()
-              .x(d => xLinearScale(d.date))
-              .y(d => yLinearScale1(d.vaccinesAdministered));
-
-          var line2 = d3
-              .line()
-              .x(d => xLinearScale(d.date))
-              .y(d => yLinearScale2(d.cases));
-
-
-          // Append a path for line1
-          chartGroup.append("#myBarChart")
-              .data([Data])
-              .attr("d", line1)
-              .attr("color", "green")
-
-          // Append a path for line2
-          chartGroup.append("myBarChart")
-              .data([Data])
-              .attr("d", line2)
-              .attr("color", "red")
-
-      }).catch(function (error) {
-          console.log(error);
-      })
 }
-
 function optionChanged(value) {
-    myBarChart(value)
-      myAreaChart(value)
-      myGauge(value)
-      myMap(value)
+  filteredState = myStateData.filter(d => d.state == value)
+  console.log(filteredState)
+  myBarChart(filteredState)
+  myAreaChart(filteredState)
+  myGauge(filteredState)
+  myMap(filteredState)
 
 }
+myStateData = [];
+
+function init() {
+  // call the functions to display the data and the plots to the page
+  d3.json("/historicalData").then(function (Data) {
+    //Point to the vaccination portion of the data file
+    myStateData = Data;
+
+
+
+  }
+  );
+  d3.json("/stateData").then((Data) => {
+    //Point to the vaccination portion of the data file
+    // var vaccinations = Data.vaccinesAdministered;
+    // console.log(vaccinations)
+
+    // var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
+    // var result = resultArray[0];
+    console.log(Data)
+    var stateList = Data.map(d => d.state);
+    var dropdownMenu = d3.selectAll("#selDataset");
+    stateList.forEach(function (state) {
+      dropdownMenu.append("option").text(state)
+        .property("value", state)
+    });
+  });
+}
+
+init();
+
+buildCharts();
--- Flask_Setup/static/js/app2.js
+++ Flask_Setup/static/js/app2.js
@@ -2,111 +2,111 @@ var queryUrlStates = `https://api.covidactnow.org/v2/states.json?apiKey=${API_KE
 var queryUrlCounties = `https://api.covidactnow.org/v2/counties.json?apiKey=${API_KEY}`
 var queryUrlCbsas = `https://api.covidactnow.org/v2/cbsas.json?apiKey=${API_KEY}`
 
-// // The code for the chart is wrapped inside a function that automatically resizes the char
-// function makeResponsive() {
+// // // The code for the chart is wrapped inside a function that automatically resizes the char
+// // function makeResponsive() {
 
-//     // Import data from the csv file
-//     d3.csv('link').then(function(data) {
+// //     // Import data from the csv file
+// //     d3.csv('link').then(function(data) {
 
-// },
+// // },
 
-// // When the browser loads, makeResponsive() is called.
-// makeResponsive();
+// // // When the browser loads, makeResponsive() is called.
+// // makeResponsive();
 
-// // When the browser window is resized, makeResponsive() is called.
-// d3.select(window).on('resize', makeResponsive);
+// // // When the browser window is resized, makeResponsive() is called.
+// // d3.select(window).on('resize', makeResponsive);
 
-function buildCharts(vaccination) {
-    d3.json("data/covid.db").then((Data) => {
-        //Point to the vaccination portion of the data file
-        var vaccinations = Data.vaccinations;
+// function buildCharts(vaccination) {
+//     d3.json("data/covid.db").then((Data) => {
+//         //Point to the vaccination portion of the data file
+//         var vaccinations = Data.vaccinations;
 
-        var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
-        var result = resultArray[0];
+//         var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
+//         var result = resultArray[0];
 
-        //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
-        var states = result.state;
-        var vaccinationsCompleted = result.VaccinationsCompleted;
-        var vaccinationsDistributed = result.vaccinationsDistributed.reverse();
-        var vaccinationsAdministered = result.vaccinationsAdministered;
-        var cases = result.cases;
+//         //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
+//         var states = result.state;
+//         var vaccinationsCompleted = result.VaccinationsCompleted;
+//         var vaccinationsDistributed = result.vaccinationsDistributed.reverse();
+//         var vaccinationsAdministered = result.vaccinationsAdministered;
+//         var cases = result.cases;
 
-        //Create Gauge Chart 
-        var data = [
-            {
-                domain: { x: [0, 1], y: [0, 1] },
-                value: vaccinationsCompleted,
-                title: { text: 'Fully Vaccinated Percentage' },
-                type: 'indicator',
-                mode: 'gauge+number',
-                gauge: {
-                    axis: { range: [null, 100] },
-                    steps: [
-                        {
-                            range: [0, 25], color: 'red',
-                            range: [25, 50], color: 'orange',
-                            range: [50, 75], color: 'yellow',
-                            range: [75, 100], color: 'green'
-                        }
-                    ],
-                    threshold: {
-                        line: { color: 'gold', width: 4 },
-                        thickness: 0.75,
-                        value: 99
-                    }
-                }
-            }
-        ];
+//         //Create Gauge Chart 
+//         var data = [
+//             {
+//                 domain: { x: [0, 1], y: [0, 1] },
+//                 value: vaccinationsCompleted,
+//                 title: { text: 'Fully Vaccinated Percentage' },
+//                 type: 'indicator',
+//                 mode: 'gauge+number',
+//                 gauge: {
+//                     axis: { range: [null, 100] },
+//                     steps: [
+//                         {
+//                             range: [0, 25], color: 'red',
+//                             range: [25, 50], color: 'orange',
+//                             range: [50, 75], color: 'yellow',
+//                             range: [75, 100], color: 'green'
+//                         }
+//                     ],
+//                     threshold: {
+//                         line: { color: 'gold', width: 4 },
+//                         thickness: 0.75,
+//                         value: 99
+//                     }
+//                 }
+//             }
+//         ];
 
-        var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+//         var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
 
-        Plotly.newPlot('myGuage', data, layout);
+//         Plotly.newPlot('myGuage', data, layout);
 
-        //Stacked Bar Chart
+//         //Stacked Bar Chart
 
-        var trace1 = {
-            x: state,
-            y: vaccinationsAdministered,
-            name: 'Vaccinations Administered',
-            type: 'bar'
-        };
+//         var trace1 = {
+//             x: state,
+//             y: vaccinationsAdministered,
+//             name: 'Vaccinations Administered',
+//             type: 'bar'
+//         };
 
-        var trace2 = {
-            x: state,
-            y: vaccinationsDistributed,
-            name: 'Vaccinations Distributed',
-            type: 'bar'
-        };
+//         var trace2 = {
+//             x: state,
+//             y: vaccinationsDistributed,
+//             name: 'Vaccinations Distributed',
+//             type: 'bar'
+//         };
 
-        var data = [trace1, trace2];
+//         var data = [trace1, trace2];
 
-        var layout = { barmode: 'stack' };
+//         var layout = { barmode: 'stack' };
 
-        Plotly.newPlot('myAreaChart', data, layout);
+//         Plotly.newPlot('myAreaChart', data, layout);
 
-        // Double Line Vaccination rate and cases vs data
-        //   var trace3 = {
-        //     x: date,
-        //     y: vaccinationsAdministered,
-        //     name: 'Vaccinations Administered',
-        //     type: 'line'
-        //   };
+//         // Double Line Vaccination rate and cases vs data
+//         //   var trace3 = {
+//         //     x: date,
+//         //     y: vaccinationsAdministered,
+//         //     name: 'Vaccinations Administered',
+//         //     type: 'line'
+//         //   };
 
-        //   var trace4 = {
-        //     x: date,
-        //     y: cases,
-        //     name: 'Cases',
-        //     type: 'line'
-        //   };
+//         //   var trace4 = {
+//         //     x: date,
+//         //     y: cases,
+//         //     name: 'Cases',
+//         //     type: 'line'
+//         //   };
 
-        //   var data = [trace3, trace4];
+//         //   var data = [trace3, trace4];
 
-        //   var layout = {
-        //       title: "Vaccination Rate vs. Case Rate",
-        //       xaxis: { title: "date"}
-        //   };
+//         //   var layout = {
+//         //       title: "Vaccination Rate vs. Case Rate",
+//         //       xaxis: { title: "date"}
+//         //   };
 
-        //   Plotly.newPlot('myBarChart', data, layout);
+//         //   Plotly.newPlot('myBarChart', data, layout);
 
         function init() {
             // call the functions to display the data and the plots to the page
@@ -145,29 +145,29 @@ function buildCharts(vaccination) {
         // d3.csv("donuts.csv").then(function(donutData) { 
 
         function buildCharts(cases) {
-            var vaccinations = Data.vaccinations;
+            var vaccinations = Data.vaccinesAdministered;
             var date = Data.date
-            d3.json("data/covid.db").then(function (Data) {
+            d3.json("/historicalData").then(function (Data) {
                 //Point to the vaccination portion of the data file
 
 
                 Data.forEach(function (Data) {
-                    var resultArray = vaccinations.filter(vaccination => vaccination == vaccinations);
+                    var resultArray = vaccinesAdministered.filter(state => state == state);
                     var result = resultArray[0];
 
                     //assign the otu_ids, sample_values, and otu_labels to variables to use in plots
                     var states = result.state;
-                    var vaccinationsCompleted = result.VaccinationsCompleted;
+                    var vaccinesAdministered = result.vaccinesAdministered;
                     var cases = result.cases;
                     var date = result.date;
                 });
 
                 var xLinearScale = d3.scaleLinear()
-                    .domain(d3.extent(Data, d => d.date))
+                    .domain(d3.max(Data, d => d.date))
                     .range([0, width]);
 
                 var yLinearScale1 = d3.scaleLinear()
-                    .domain([0, d3.max(Data, d => d.vaccinationsCompleted)])
+                    .domain([0, d3.max(Data, d => d.vaccinesAdministered)])
                     .range([height, 0]);
 
                 var yLinearScale2 = d3.scaleLinear()
@@ -188,17 +188,17 @@ function buildCharts(vaccination) {
 
                 var line1 = d3
                     .line()
-                    .x(d => xTimeScale(d.date))
-                    .y(d => yLinearScale1(d.vaccinationsCompleted));
+                    .x(d => xLinearScale(d.date))
+                    .y(d => yLinearScale1(d.vaccinesAdministered));
 
                 var line2 = d3
                     .line()
-                    .x(d => xTimeScale(d.date))
+                    .x(d => xLinearScale(d.date))
                     .y(d => yLinearScale2(d.cases));
 
 
                 // Append a path for line1
-                chartGroup.append("myBarChart")
+                chartGroup.append("#myBarChart")
                     .data([Data])
                     .attr("d", line1)
                     .attr("color", "green")
@@ -213,4 +213,3 @@ function buildCharts(vaccination) {
                 console.log(error);
             })
     }
-})}
